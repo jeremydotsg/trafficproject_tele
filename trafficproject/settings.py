78a1d73e36db0a218a19cf05a1c2c224cdbf0dd2
jeremydotsg <weiqiang@gmail.com>
2024-05-28 02:07:36 +0800

    Removed admin generator

--- trafficproject/settings.py
+++ trafficproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_admin_generator',
+    #'django_admin_generator',
 ]
 
 MIDDLEWARE = [
