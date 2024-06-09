fa744a9c2cb6d3103ab79eab2fd0684183d86343
jeremydotsg <weiqiang@gmail.com>
2024-06-09 13:28:53 +0800

    Add in IP filtering

--- trafficdb/middleware.py
+++ trafficdb/middleware.py
@@ -1,20 +1,34 @@
 from django.http import HttpResponseForbidden
+import requests
 
 class BlockNonLocalMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-
+    
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        print(request.META.get('X_REAL_IP'))
-        # Block users who are not from the local host
-        #if not request.META.get('X_REAL_IP') == '127.0.0.1':
-        #    return HttpResponseForbidden()
-
-        response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
-        return response
+        # Get the client's IP address
+        ip = None
+        if request:
+            real_ip = request.META.get("X_REAL_IP", "")
+            remote_ip = request.META.get("REMOTE_ADDR", "")
+            forwarded_ip = request.META.get("HTTP_X_FORWARDED_FOR", "")
+            print('Views - Real IP: ' + str(real_ip) + ', Remote IP: ' + str(remote_ip) + ', Forwarded IP: ' + str(forwarded_ip))
+            if not real_ip:
+                if not forwarded_ip:
+                    ip = remote_ip
+                else:
+                    ip = forwarded_ip
+            else:
+                ip = real_ip
+        
+        # Check the IP's country using an IP geolocation API
+        print(ip)
+        response = requests.get(f'http://ip-api.com/json/{ip}')
+        if response.status_code == 200:
+            country = response.json().get('country')
+            print(country)
+            if country not in ['Singapore','Malaysia']:
+                return HttpResponseForbidden('<h1>Access Denied</h1>')
+        
+        # If the IP is from Singapore or Malaysia, or if the API call failed, continue processing
+        return self.get_response(request)
