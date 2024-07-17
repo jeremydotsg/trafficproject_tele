c40ab9c0d78ea9a1f5314c523ad4b950ceda5bfc
Jeremy <weiqiang@gmail.com>
2024-07-17 22:50:32 +0800

    Revert "Initial Commit."
    This reverts commit e363d5a158cbd9f1c61d8814180b064eeba6504e.

--- trafficproject/bus_stop.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Assuming you have a JSON file named 'data.json'
-with open('datadump.json', 'r') as file:
-    data = json.load(file)
-
-# Now 'data' is a Python dictionary that contains the data from the JSON file.
-print(data)
--- trafficproject/trafficdb/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrafficdbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trafficdb'
