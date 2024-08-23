041056d24f929531285b7d4cac7c4941e7796b4d
Jeremy <weiqiang@gmail.com>
2024-08-23 20:40:39 +0800

    Temporarily removing blocknonlocal middleware

--- trafficproject/settings.py
+++ trafficproject/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'trafficdb.middleware.BlockNonLocalMiddleware',
+    #'trafficdb.middleware.BlockNonLocalMiddleware',
 ]
 
 ROOT_URLCONF = 'trafficproject.urls'
