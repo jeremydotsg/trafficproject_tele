024fba85393aec026b0eabf1ba5b0078978b8c33
jeremydotsg <56425771+jeremydotsg@users.noreply.github.com>
2024-08-25 23:05:15 +0800

    Update urls.py

--- trafficproject/urls.py
+++ trafficproject/urls.py
@@ -36,7 +36,6 @@ urlpatterns = [
 ] 
 
 
-if settings.DEBUG or os.getenv('ENVIRONMENT') in ['prod_koyeb']:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
