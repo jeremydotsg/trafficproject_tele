dc37a84e04687988792a543637c786a0311591de
Jeremy <weiqiang@gmail.com>
2024-08-27 21:01:45 +0800

    Remove disable dev shm.

--- trafficdb/extract_text.py
+++ trafficdb/extract_text.py
@@ -13,7 +13,7 @@ def get_rate():
     chrome_options.add_argument('--disable-extensions')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
 
 
     # Initialize the Chrome driver with the specified options
